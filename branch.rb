860de0a3163e82074bd946fe8001e5a31b4c8518
Nick Barth <nbarth@goauto.ca>
2018-01-08 02:13:12 -0700

    fixed pkg

--- branch.rb
+++ branch.rb
@@ -1,9 +1,9 @@
 class Branch < Formula
   desc "branch is sample utility for switching branches using chooser."
   homepage "https://github.com/nickbarth/branch"
-  url "https://github.com/nickbarth/branch/archive/v0.1-alpha.tar.gz"
-  version "1.0.0"
-  sha256 "955ba22959bfa181aa79810e4094713e95f679ff21059eff910977e265d33866"
+  url "https://github.com/nickbarth/branch/releases/download/v0.1-alpha/branch"
+  version "0.0.1"
+  sha256 "f7aa096445711e7bb7da113619e1598ded816e1531a494d7b610ce82d012d1d5"
 
   def install
     bin.install "branch"
