571a5cb5ecaa337d844b185ea01ea42618985134
Nick Barth <nbarth@goauto.ca>
2018-01-30 22:09:21 -0700

    updated homebrew release version

--- branch.rb
+++ branch.rb
@@ -1,9 +1,9 @@
 class Branch < Formula
   desc "branch is sample utility for switching branches using chooser."
   homepage "https://github.com/nickbarth/branch"
-  url "https://github.com/nickbarth/branch/releases/download/v0.1-alpha/branch"
-  version "0.0.1"
-  sha256 "f7aa096445711e7bb7da113619e1598ded816e1531a494d7b610ce82d012d1d5"
+  url "https://github.com/nickbarth/branch/releases/download/v0.2-alpha/branch"
+  version "0.2.0"
+  sha256 "8acc76cc03fb4d991bbeabdda39ae51d8b6bd12cfcd3b7dbc41b74af1d3df9a7"
 
   def install
     bin.install "branch"
